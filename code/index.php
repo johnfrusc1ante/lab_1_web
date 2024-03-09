501f897150522fe3077c320cdf22b8705dafe2a7
johnfrusc1ante <161211583+johnfrusc1ante@users.noreply.github.com>
2024-03-09 20:33:17 +0200

    Update index.php

--- code/index.php
+++ code/index.php
@@ -3,3 +3,14 @@ $very_bad_unclear_name = "s proshedshim ";
 $order = &$vary_bad_unclear_name;
 $order = $order."8 marta";
 echo "\nyou have 1 unread message: $very_bad_unclear_name\n";
+
+$a = 4;
+echo $a;
+$b = 3.14;
+echo "\n";
+echo $b;
+echo "\n";
+echo 9+3;
+$last_month = 1187.23;
+$this_month = 1089.98;
+echo $last_month-$this_month;

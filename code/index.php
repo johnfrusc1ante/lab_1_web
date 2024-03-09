480a33bfb82ccff000eb172c93eeb3bfec0248d9
johnfrusc1ante <161211583+johnfrusc1ante@users.noreply.github.com>
2024-03-09 20:31:10 +0200

    Update index.php

--- code/index.php
+++ code/index.php
@@ -1,2 +1,5 @@
 <?php
-echo phpinfo();
+$very_bad_unclear_name = "s proshedshim ";
+$order = &$vary_bad_unclear_name;
+$order = $order."8 marta";
+echo "\nyou have 1 unread message: $very_bad_unclear_name\n";

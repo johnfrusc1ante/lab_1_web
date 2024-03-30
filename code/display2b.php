86a195e96befb3db9a5c8c9b66e747064ff3cbf3
johnfrusc1ante <161211583+johnfrusc1ante@users.noreply.github.com>
2024-03-30 20:24:16 +0200

    Update display2b.php

--- code/display2b.php
+++ code/display2b.php
@@ -6,7 +6,23 @@ $name = isset($_SESSION['name']) ? $_SESSION['name'] : '';
 $age = isset($_SESSION['age']) ? $_SESSION['age'] : '';
 
 if (empty($surname) || empty($name) || empty($age)) {
-    header('Location: formSNA.php');
+    header('Location: index2b.php');
     exit;
 }
 ?>
+
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Данные пользователя</title>
+</head>
+<body>
+    <h1>Данные пользователя</h1>
+    <p><strong>Фамилия:</strong> <?php echo $surname; ?></p>
+    <p><strong>Имя:</strong> <?php echo $name; ?></p>
+    <p><strong>Возраст:</strong> <?php echo $age; ?></p>
+</body>
+</html>

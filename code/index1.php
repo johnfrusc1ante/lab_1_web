5e861eedabfe37447b5e19d4018db23a5eec8a35
johnfrusc1ante <161211583+johnfrusc1ante@users.noreply.github.com>
2024-03-30 19:32:12 +0200

    Update index1.php

--- code/index1.php
+++ code/index1.php
@@ -9,3 +9,14 @@ foreach($matches[0] as $i) {
     echo $i."<br>";
 }
 
+#1.b
+
+$str = 'a1b2c3';
+
+function degree($matches) {
+    return pow($matches[0], 3);
+}
+
+$new_str = preg_replace_callback('/\d+/', 'degree', $str);
+
+echo $new_str;

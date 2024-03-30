770c8bff7ede154e2e00f9df767b154c0f76e0c0
johnfrusc1ante <161211583+johnfrusc1ante@users.noreply.github.com>
2024-03-30 20:45:14 +0200

    Update display2c.php

--- code/display2c.php
+++ code/display2c.php
@@ -1,1 +1,12 @@
 <?php
+session_start();
+
+$user_data = isset($_SESSION['user_data']) ? $_SESSION['user_data'] : array();
+
+if (empty($user_data)) {
+    header('Location: index2c.php');
+    exit;
+}
+?>
+
+

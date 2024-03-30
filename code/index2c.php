64f44120e3250672b786bcf8910d610c6cee97e6
johnfrusc1ante <161211583+johnfrusc1ante@users.noreply.github.com>
2024-03-30 20:30:51 +0200

    Update index2c.php

--- code/index2c.php
+++ code/index2c.php
@@ -1,1 +1,16 @@
 <?php
+session_start();
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $data = array(
+        'name' => $_POST['name'],
+        'age' => $_POST['age'],
+        'salary' => $_POST['salary'],
+        'country_of_origin' => $_POST['country_of_origin']
+    );
+
+    $_SESSION['user_data'] = $data;
+    header('Location: display2c.php');
+    exit;
+}
+?>

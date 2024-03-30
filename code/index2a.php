9969257737ba818fa5b4962cc553e5dac853f925
johnfrusc1ante <161211583+johnfrusc1ante@users.noreply.github.com>
2024-03-30 19:50:37 +0200

    Update index2a.php

--- code/index2a.php
+++ code/index2a.php
@@ -11,3 +11,25 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $char_count = 0;
 }
 ?>
+
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Счетчик слов и символов</title>
+</head>
+<body>
+<form method="post">
+    <textarea name="text"><?php echo $str; ?></textarea>
+    <br>
+    <button type="submit">Подсчитать слова и символы</button>
+</form>
+
+<?php if ($_SERVER['REQUEST_METHOD'] == 'POST'): ?>
+    <p>Число слов: <?php echo $word_count; ?></p>
+    <p>Число символов: <?php echo $char_count; ?></p>
+<?php endif; ?>
+</body>
+</html>

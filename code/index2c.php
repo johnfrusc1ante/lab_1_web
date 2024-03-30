b235414a58722dd28b8361863b67b2c74011d605
johnfrusc1ante <161211583+johnfrusc1ante@users.noreply.github.com>
2024-03-30 20:33:34 +0200

    Update index2c.php

--- code/index2c.php
+++ code/index2c.php
@@ -14,3 +14,12 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     exit;
 }
 ?>
+
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Данные пользователя</title>
+</head>

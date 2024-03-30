c76d6e4497d47de76c8b9d21ef3605c56bca5903
johnfrusc1ante <161211583+johnfrusc1ante@users.noreply.github.com>
2024-03-30 20:04:04 +0200

    Update index2b.php

--- code/index2b.php
+++ code/index2b.php
@@ -1,1 +1,11 @@
 <?php
+session_start();
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $_SESSION['surname'] = $_POST['surname'];
+    $_SESSION['name'] = $_POST['name'];
+    $_SESSION['age'] = $_POST['age'];
+    header('Location: display.php');
+    exit;
+}
+?>

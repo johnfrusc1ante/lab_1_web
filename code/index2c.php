8120a7b3180109248c0ac029db73f1f42f4578bb
johnfrusc1ante <161211583+johnfrusc1ante@users.noreply.github.com>
2024-03-30 20:40:36 +0200

    Update index2c.php

--- code/index2c.php
+++ code/index2c.php
@@ -23,3 +23,21 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Данные пользователя</title>
 </head>
+<body>
+    <form method="post">
+        <label for="name">Имя:</label>
+        <input type="text" name="name" id="name" required>
+        <br>
+        <label for="age">Возраст:</label>
+        <input type="number" name="age" id="age" required>
+        <br>
+        <label for="salary">Зарплата:</label>
+        <input type="text" name="salary" id="salary" required>
+        <br>
+        <label for="marriage_condition">Состояние в браке:</label>
+        <input type="text" name="country_of_origin" id="country_of_origin" required>
+        <br>
+        <button type="submit">Submit</button>
+    </form>
+</body>
+</html>

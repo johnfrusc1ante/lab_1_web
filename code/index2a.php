cad255d029a13520b4766b48c8886379642d7b4a
johnfrusc1ante <161211583+johnfrusc1ante@users.noreply.github.com>
2024-03-30 19:40:38 +0200

    Update index2a.php

--- code/index2a.php
+++ code/index2a.php
@@ -1,1 +1,13 @@
 <?php
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+    $str = $_POST['text'];
+
+    $word_count = str_word_count($str);
+    $char_count = strlen($str);
+} else {
+    $str = '';
+    $word_count = 0;
+    $char_count = 0;
+}
+?>

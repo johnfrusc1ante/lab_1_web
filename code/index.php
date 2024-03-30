c0be1580949bd3ea6008ad80994577aaedf39d1e
johnfrusc1ante <161211583+johnfrusc1ante@users.noreply.github.com>
2024-03-30 18:01:13 +0200

    Update index.php

--- code/index.php
+++ code/index.php
@@ -7,3 +7,22 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Avito</title>
 </head>
+<body>
+<div id="form">
+    <form action="save.php" method="post">
+        <label for="email">Email</label><br>
+        <input type="email" name="email" required><br><br>
+        <label for="category">Category</label>
+        <select name="category" required>
+<?php
+$dirPath = "./categories/";
+if (is_dir($dirPath))
+{
+    $subDirs = array_filter(glob($dirPath . '*'), 'is_dir');
+    foreach ($subDirs as $subDir)
+    {
+        $name = basename($subDir);
+        echo "<option value=\"$name\">$name</option>";
+    }
+}
+?>

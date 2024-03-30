acc47d5cc68c69ea27827028759c46a65c6f64b4
johnfrusc1ante <161211583+johnfrusc1ante@users.noreply.github.com>
2024-03-30 18:44:50 +0200

    Update save.php

--- code/save.php
+++ code/save.php
@@ -4,3 +4,7 @@ function redirectToHome()
     header("Location: index.php");
     exit();
 }
+
+if (false === isset($_POST['email'], $_POST['category'], $_POST['title'], $_POST['description'])) {
+    redirectToHome();
+}

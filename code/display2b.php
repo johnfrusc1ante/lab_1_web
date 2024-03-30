45f25a1c8ad8a975081396fd82400ff27c7304f1
johnfrusc1ante <161211583+johnfrusc1ante@users.noreply.github.com>
2024-03-30 20:17:25 +0200

    Update display2b.php

--- code/display2b.php
+++ code/display2b.php
@@ -1,1 +1,12 @@
 <?php
+session_start();
+
+$surname = isset($_SESSION['surname']) ? $_SESSION['surname'] : '';
+$name = isset($_SESSION['name']) ? $_SESSION['name'] : '';
+$age = isset($_SESSION['age']) ? $_SESSION['age'] : '';
+
+if (empty($surname) || empty($name) || empty($age)) {
+    header('Location: formSNA.php');
+    exit;
+}
+?>

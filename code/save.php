78e4cbd942cf54c1807704af8d303dcbb9c0cdf0
johnfrusc1ante <161211583+johnfrusc1ante@users.noreply.github.com>
2024-03-30 18:51:50 +0200

    Update save.php

--- code/save.php
+++ code/save.php
@@ -8,3 +8,14 @@ function redirectToHome()
 if (false === isset($_POST['email'], $_POST['category'], $_POST['title'], $_POST['description'])) {
     redirectToHome();
 }
+
+$category = $_POST['category'];
+$email = $_POST['email'];
+$title = $_POST['title'];
+$desc = $_POST['description'];
+$category_folder = "./categories/$category";
+$file_path = "$category_folder/$title.txt";
+$content = "$email\n$title\n$desc";
+file_put_contents($file_path, $content);
+redirectToHome();
+?>

fa5b5ad7f7af40d6107e0aee62144506b6edb7d2
johnfrusc1ante <161211583+johnfrusc1ante@users.noreply.github.com>
2024-03-30 20:47:56 +0200

    Update display2c.php

--- code/display2c.php
+++ code/display2c.php
@@ -9,4 +9,20 @@ if (empty($user_data)) {
 }
 ?>
 
-
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Данные пользователя</title>
+</head>
+<body>
+    <h1>Данные пользователя</h1>
+    <ul>
+        <?php foreach ($user_data as $key => $value) : ?>
+            <li><strong><?php echo $key; ?>:</strong> <?php echo $value; ?></li>
+        <?php endforeach; ?>
+    </ul>
+</body>
+</html>

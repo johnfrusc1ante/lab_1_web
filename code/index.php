dd5ea63464e7470706bb74960fb088ee4873c53f
johnfrusc1ante <161211583+johnfrusc1ante@users.noreply.github.com>
2024-05-03 20:42:38 +0200

    Update index.php

--- code/index.php
+++ code/index.php
@@ -34,3 +34,14 @@
             <th>Title</th>
             <th>Description</th>
         </tr>
+        </thead>
+        <tbody>
+            <?php
+            require __DIR__ . '/vendor/autoload.php';
+
+            $client = new \Google_Client();
+            $client->setApplicationName('lab4');
+            $client->setScopes(['https://www.googleapis.com/auth/spreadsheets']);
+            $client->setAccessType('offline');
+            $path = __DIR__ . '/credentials.json';
+            $client->setAuthConfig($path);

5d904d8f7281d5daddc47337c06761ae373e709f
johnfrusc1ante <161211583+johnfrusc1ante@users.noreply.github.com>
2024-05-03 20:48:46 +0200

    Update save.php

--- code/save.php
+++ code/save.php
@@ -1,1 +1,9 @@
 <?php
+require __DIR__ . '/vendor/autoload.php';
+
+$client = new \Google_Client();
+$client->setApplicationName('lab4');
+$client->setScopes(['https://www.googleapis.com/auth/spreadsheets']);
+$client->setAccessType('offline');
+$path = __DIR__ . '/credentials.json';
+$client->setAuthConfig($path);

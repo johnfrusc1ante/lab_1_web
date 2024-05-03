f9428f12c79e0aae08e5b1539815889bfe2b69e2
johnfrusc1ante <161211583+johnfrusc1ante@users.noreply.github.com>
2024-05-03 20:52:49 +0200

    Update save.php

--- code/save.php
+++ code/save.php
@@ -7,3 +7,31 @@ $client->setScopes(['https://www.googleapis.com/auth/spreadsheets']);
 $client->setAccessType('offline');
 $path = __DIR__ . '/credentials.json';
 $client->setAuthConfig($path);
+
+$service = new Google\Service\Sheets($client);
+
+$spreadsheetId = '1rHYXEr1rgsnArbYia0_lEY5NyK427HFHDt4xVjVVQ0c';
+
+$email = $_POST["email"];
+$category = $_POST["category"];
+$title = $_POST["title"];
+$description = $_POST['description'];
+
+$values = [
+    [$email, $category, $title, $description]
+];
+
+$range = 'lab4' . '!A:D';
+
+$body = new Google_Service_Sheets_ValueRange([
+    'values' => $values
+]);
+$params = [
+    'valueInputOption' => 'RAW'
+];
+
+$result = $service->spreadsheets_values->append($spreadsheetId, $range, $body, $params);
+
+header('Location: index.php');
+exit();
+?>

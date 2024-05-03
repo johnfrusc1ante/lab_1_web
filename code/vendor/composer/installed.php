b6be848a2430e568733798c36262640f6f114f5b
johnfrusc1ante <161211583+johnfrusc1ante@users.noreply.github.com>
2024-05-03 19:42:13 +0200

    seting up

---
+++ code/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'snyat/code',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'bff538ebf667a89c54475e481e34c08cc07f8a59',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'snyat/code' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'bff538ebf667a89c54475e481e34c08cc07f8a59',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);

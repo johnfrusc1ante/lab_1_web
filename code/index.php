cf63ac086a3defff7c07f8ea791539d3ded24334
johnfrusc1ante <161211583+johnfrusc1ante@users.noreply.github.com>
2024-06-04 10:27:27 +0200

    Update index.php

--- code/index.php
+++ code/index.php
@@ -24,5 +24,16 @@ if ($result = $mysqli->query('SELECT * FROM ad ORDER BY created DESC')) {
     $result->close();
 }
 $mysqli->close();
-
 ?>
+
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>lab_5</title>
+</head>
+<body>
+<h1>The table</h1>

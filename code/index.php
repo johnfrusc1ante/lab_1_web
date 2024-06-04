08c8a0fad326bce9ebcdc26525bc86b272d80b14
johnfrusc1ante <161211583+johnfrusc1ante@users.noreply.github.com>
2024-06-04 10:20:45 +0200

    Create index.php

---
+++ code/index.php
@@ -0,0 +1,17 @@
+<?php
+//Подключение к mysql
+$mysqli = new mysqli('db', 'root', 'helloworld', 'web');
+
+if (mysqli_connect_errno()) {
+    printf("Подключение к серверу MySQL невозможно. Код ошибки: %s\n", mysqli_connect_error());
+    exit;
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $email = $mysqli->real_escape_string($_POST['email']);
+    $title = $mysqli->real_escape_string($_POST['title']);
+    $category = $mysqli->real_escape_string($_POST['categories']);
+    $description = $mysqli->real_escape_string($_POST['text']);
+
+    $mysqli->query("INSERT INTO ad (email, title, description, category) VALUES ('$email', '$title', '$description', '$category')");
+}
